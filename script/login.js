237c1cccc345142ec32137966fb23616f460d4a5
kavitha <kavinu2010@gmail.com>
2024-12-23 14:35:18 +0100

    added url endpoints

--- script/login.js
+++ script/login.js
@@ -1,3 +1,16 @@
 let u_name = document.getElementById("username");
-let pass = document.getElementById("password");
+let u_pass = document.getElementById("password");
 let submitBtn = document.getElementById("submit");
+
+submitBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  let user = u_name.value;
+  let pass = u_pass.value;
+
+  let userProm = authUser(user, pass);
+});
+
+function authUser(user, pass) {
+  let url = "https://dummyjson.com/auth/login";
+  let prom = fetch(url);
+}
